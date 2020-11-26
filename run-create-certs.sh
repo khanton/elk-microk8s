8fb03c33d34caccd8d7e4d2f475c6de1e9b33b75
Anton Kholodkov <khanton@yandex.ru>
2020-11-26 22:16:52 +0300

    Update run-create-certs.sh

--- run-create-certs.sh
+++ run-create-certs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export STACK_VERSION=7.10.0
 export DNS_NAME=es-master
-export ELASTIC_PASSWORD=F9f4CVyB6
+export ELASTIC_PASSWORD='Change me!'
 export NAMESPACE=default
 export KUBECTL=microk8s.kubectl
 ./create-elastic-certificates.sh
